65c75ca142cc8eba6baf75463ad1bb3bcd504ea3
Arpad Rimmel <arpad.rimmel@free.fr>
2013-04-08 18:47:53 +0200

    computestats handles several score

--- computeStats.rb
+++ computeStats.rb
@@ -20,7 +20,7 @@ end
 def launchProg()
     cmd = $nameProg
     value = `#{cmd}`
-    res = value.scan(/Score: (\d*)/)[0][0].to_i   # res is the first (and only) match
+    res = value.scan(/Score: (\d*)/)   # res is the first (and only) match
     return res
 end
 
@@ -39,26 +39,41 @@ def ciArray(list)
     return 1.96*s/Math.sqrt(list.size)
 end
 
+def printRes(listRes,listTime)
+    for i in 0...listRes.length
+        puts meanArray(listRes[i]).to_s + " +- " + ciArray(listRes[i]).to_s # + " (" + listRes[i].length.to_s + ")"
+    end
+    puts meanArray(listTime).to_s + "s"
+end
+
 begin
 
-listRes = []
-listTime = []
-puts "coucou"
-for i in 1..nbrun
-    printf("\r%i / %i",i,nbrun )
-    STDOUT.flush
-    startt = Time.now
-    res = launchProg()
-    listRes.push(res)
-    endt = Time.now
-    listTime.push(endt-startt)
-end
-puts ""
-puts meanArray(listRes).to_s + " +- " + ciArray(listRes).to_s + " (" + meanArray(listTime).to_s + "s)"
+    listRes = []
+    listTime = []
+    puts "coucou"
+    for i in 1..nbrun
+        printf("\r%i / %i",i,nbrun )
+        STDOUT.flush
+        startt = Time.now
+        res = launchProg()
+        if (res.length>listRes.length)
+            for j in listRes.length...res.length
+                listRes[j]=[]
+            end
+        end
+        for j in 0...res.length
+            listRes[j].push(res[j][0].to_i)
+        end
+        endt = Time.now
+        listTime.push(endt-startt)
+    end
+    puts ""
+    printRes(listRes,listTime)
+
 rescue Exception => e
     puts ""
     if (i>2)
-        puts meanArray(listRes).to_s + " +- " + ciArray(listRes).to_s + " (" + meanArray(listTime).to_s + "s)"
+        printRes(listRes,listTime)
     else
         puts "not enough stats"
     end
