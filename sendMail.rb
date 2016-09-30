fec18dcde08363f409449eb3f28e7f4bc23c8af0
Arpad Rimmel <arpad.rimmel@free.fr>
2016-09-30 15:47:37 +0200

    not much

--- sendMail.rb
+++ sendMail.rb
@@ -25,7 +25,6 @@ MESSAGE_END
 
 smtp = Net::SMTP.new('smtp.gmail.com',587)
 smtp.enable_starttls
-
 smtp.start('gmail.com','mail.sender.2245@gmail.com', 'pass2245', :login)
 smtp.send_message message, 'mail.sender@gmail.com', 'arpad.rimmel@gmail.com'
 smtp.finish
